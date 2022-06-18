5656dd84402fdfc6529e63393a8de373ce970e71
Caio Henrique <caiocoxim10@gmail.com>
2022-06-17 23:29:22 -0400

    [NEW] base button

---
+++ src/components/buttons/BaseButton.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+
+const BaseButton = (props) => {
+  const customStyles  = StyleSheet.create({
+    container: {
+      width: props.width,
+      backgroundColor: props.bgColor,
+      marginTop: props.marginTop,
+      marginRight: props.marginRight,
+      marginBottom: props.marginBottom,
+      marginLeft: props.marginLeft,
+      alignSelf: props.alignSelf,
+    },
+    text: {
+      color: props.textColor,
+    }
+  });
+
+  return (
+    <TouchableOpacity style={[styles.button, customStyles.container]} onPress={props.onPress}>
+      <Text style={[styles.text, customStyles.text]}>{props.text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    borderRadius: 4,
+    width: "90%",
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
+  },
+});
+
+export default BaseButton;
